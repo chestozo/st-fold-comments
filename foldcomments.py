9aee7f2c94cd1c01d36da15eee59d4b8da72dfc4
chestozo <chestozo@yandex-team.ru>
2013-04-18 15:53:06 +0400

    unfold command

--- foldcomments.py
+++ foldcomments.py
@@ -40,22 +40,13 @@ def fold_comments(self):
 
     self.view.fold(regions)
 
-
-# FIXME fold comments by default...
-# class FoldFileComments(sublime_plugin.EventListener):
-#     def on_load(self, view):
-#         view.fold(view.find_by_selector('comment'))
+def unfold_comments(self):
+    self.view.unfold(self.view.find_by_selector('comment'))
 
 class FoldCommentsCommand(sublime_plugin.TextCommand):
     def run(self, edit):
         fold_comments(self)
 
-
-# class UnfoldCommentsCommand(sublime_plugin.TextCommand):
-#     def run(self, edit):
-#         self.view.unfold(
-#             map(
-#                 (lambda x: sublime.Region(x.a, x.b - (0 if self.view.substr(sublime.Region(x.b - 2, x.b)) == '*/' else 1))),
-#                 self.view.find_by_selector('comment')
-#             )
-#         )
+class UnfoldCommentsCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        unfold_comments(self)

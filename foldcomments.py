87adec26e45083d723e5a24e8f253fd4958ea71c
chestozo <chestozo@yandex-team.ru>
2013-04-18 14:33:46 +0400

    Update to current version

--- foldcomments.py
+++ foldcomments.py
@@ -6,9 +6,18 @@ class FoldFileComments(sublime_plugin.EventListener):
 
 class FoldCommentsCommand(sublime_plugin.TextCommand):
     def run(self, edit):
-        self.view.fold(self.view.find_by_selector('comment'))
+        self.view.fold(
+            map(
+                (lambda x: sublime.Region(x.a, x.b - (0 if self.view.substr(sublime.Region(x.b - 2, x.b)) == '*/' else 1))),
+                self.view.find_by_selector('comment')
+            )
+        )
 
 class UnfoldCommentsCommand(sublime_plugin.TextCommand):
     def run(self, edit):
-        for region in self.view.find_by_selector('comment'):
-            self.view.unfold(region)
+        self.view.unfold(
+            map(
+                (lambda x: sublime.Region(x.a, x.b - (0 if self.view.substr(sublime.Region(x.b - 2, x.b)) == '*/' else 1))),
+                self.view.find_by_selector('comment')
+            )
+        )
